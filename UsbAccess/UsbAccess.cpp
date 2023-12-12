3f006822714d6f0c61f7ddf92178cfcbfa148950
dponna534 <Deepak_Ponnath2@comcast.com>
2023-12-12 04:44:35 +0000

    RDKTV-26935-fix

--- UsbAccess/UsbAccess.cpp
+++ UsbAccess/UsbAccess.cpp
@@ -584,7 +584,8 @@ namespace Plugin {
             if(!usbPath.empty())
             {
                 string script = (ARCHIVE_LOGS_SCRIPT + " " + usbPath);
-                FILE* fp =  v_secure_popen("r","%s",script.c_str());
+		LOGINFO("Secure open arugument fix for script and parameter");
+                FILE* fp =  v_secure_popen("r","/lib/rdk/usbLogUpload.sh %s",usbPath.c_str());
                 if (NULL != fp) {
                     char buf[256];
                     while(fgets(buf, sizeof(buf), fp) != NULL)
